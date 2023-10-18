fd3f0984bf8186cfdeed8f08d14407591bb83573
Valerya <13588262185@163.com>
2023-10-18 04:50:11 -0700

    Add level
    Add level

--- Assets/Scripts/ShowTutorial.cs
+++ Assets/Scripts/ShowTutorial.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ShowTutorial : MonoBehaviour
 {
     public GameObject tutorialText;
+    public GameObject level;
+    public GameObject hint;
     // Start is called before the first frame update
     void Start()
     {
+        level.GetComponentInChildren<TMP_Text>().text = string.Concat("Level ", SceneManager.GetActiveScene().buildIndex + 1);
         if (SceneManager.GetActiveScene().buildIndex > 0)
         {
-            tutorialText.SetActive(false);
+            hint.GetComponentInChildren<TMP_Text>().text = "Press [Enter] to continue!";
         }
     }
 
