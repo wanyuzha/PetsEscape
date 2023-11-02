ad7e99ed789d45b08691b72ef1a8eec14dc6be31
Wanyu Z <wanyuzha@usc.edu>
2023-11-02 04:52:53 -0700

    add transition

---
+++ Assets/Scripts/UI/ImageFade.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour
+{
+    public float fadeInTime = 1.5f;
+    public float fadeOutTime = 1.5f;
+    public float displayTime = 1.5f;
+
+    void Start()
+    { 
+        StartCoroutine(FadeImage());
+    }
+
+    IEnumerator FadeImage()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+
+        if (canvasGroup == null) yield break;
+
+        // 
+        canvasGroup.alpha = 1f;
+        // 
+        yield return new WaitForSeconds(displayTime);
+
+        // 
+        float startTime = Time.time;
+        while (Time.time < startTime + fadeOutTime)
+        {
+            float elapsed = Time.time - startTime;
+            canvasGroup.alpha = 1 - (elapsed / fadeOutTime);
+            yield return null;
+        }
+        
+        // 
+        canvasGroup.alpha = 0f;
+    }
+}
+
