53e34a85a9086edcbfb4d18ea182eae34453b92b
Yuchen Peng <yuch.pp@gmail.com>
2023-11-02 18:23:32 -0700

    change player collider

--- Assets/Scripts/Water/FloatObjectController.cs
+++ Assets/Scripts/Water/FloatObjectController.cs
@@ -5,60 +5,61 @@ using UnityEngine;
 
 public class FloatObjectController : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
-
+    private Rigidbody2D rb;
     // public GameObject bubble;
 
     private float force;
     private float waterHeight;
-    private float mass;
+    // private float mass;
     public GameObject waterArea;
-    private float floatObjectHeight;
-    bool underWater = false;
+    // private float floatObjectHeight;
+    // bool underWater = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        BoxCollider2D boxCollider;boxCollider = GetComponent<BoxCollider2D>();
-        //waterHeight = waterArea.GetComponent<DynamicWater2D>().curHeight + waterArea.GetComponent<DynamicWater2D>().bound.bottom + waterArea.transform.position.y;
-        waterHeight = waterArea.GetComponent<DynamicWater2D>().bound.top+0.5f;
-        mass = rigidBody.mass;
-        floatObjectHeight = boxCollider.size.y;
-
+        rb = GetComponent<Rigidbody2D>();
+        // BoxCollider2D boxCollider;
+        // boxCollider = GetComponent<BoxCollider2D>();
+        waterHeight = waterArea.GetComponent<DynamicWater2D>().curHeight + waterArea.GetComponent<DynamicWater2D>().bound.bottom + waterArea.transform.position.y + 0.5f;
+        // waterHeight = waterArea.GetComponent<DynamicWater2D>().bound.top + 0.5f;
+        // mass = rb.mass;
+        // floatObjectHeight = boxCollider.size.y;
     }
 
     /*
     Apply buoyancy to objects in water, and remove the force once the object is out of water.
     */
-    void LateUpdate()
+    void FixedUpdate()
     {
-
         //force = -1 * Physics.gravity.y * 3f;
-        
         float difference = transform.position.y - waterHeight;
-        Vector3 force = Vector3.up * 2f* mass * Math.Abs(difference);
         if (difference < 0)
-        {   
-            rigidBody.AddForce(force, ForceMode2D.Force);
-            underWater = true;
-            SwitchState(underWater);
-        }
-        else{
-            underWater = false;
-            SwitchState(underWater);
-        }
-        //Debug.Log(waterHeight);
-
-    }
-    void SwitchState(bool isUnderWater)
-    {
-        if(isUnderWater)
         {
-            rigidBody.drag = 5f;
-        }else
+            Vector2 force = Vector2.up * 10f * rb.mass * Math.Abs(difference);
+            rb.AddForce(force, ForceMode2D.Force);
+            // underWater = true;
+            // SwitchState(underWater);
+            rb.drag = 10f;
+        }
+        else
         {
-            rigidBody.drag = 0f;
+            // underWater = false;
+            // SwitchState(underWater);
+            rb.drag = 0f;
         }
     }
+    /*
+        void SwitchState(bool isUnderWater)
+        {
+            if (isUnderWater)
+            {
+                rb.drag = 5f;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
+        }
+    */
 }
